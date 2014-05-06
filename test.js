db191f1ce07892eafbc4ae21a894a70faed7d28a
Arne Schubert <atd.schubert@gmail.com>
2014-05-06 17:17:40 +0200

    correct spelling etc.

--- test.js
+++ test.js
@@ -5,20 +5,17 @@ var fs = require('fs');
 
 var w = new Webcheck();
 w.analyzer.use(Webcheck.middlewares.analyzer.followRedirects());
-//w.analyzer.use(Webcheck.middlewares.analyzer.followHTML({filter:/https?:\/\/[a-z]*.?hype|ruhmesmeile/}));
-w.analyzer.use(Webcheck.middlewares.analyzer.followHTML({filter:/ruhmesmeile.com/}));
-w.analyzer.use(Webcheck.middlewares.analyzer.followCSS({filter:/ruhmesmeile.com/}));
+w.analyzer.use(Webcheck.middlewares.analyzer.followHTML({filter:/^http:\/\/www.ccc.de\//}));
+w.analyzer.use(Webcheck.middlewares.analyzer.followCSS({filter:/^http:\/\/www.ccc.de\//}));
 w.analyzer.use(Webcheck.middlewares.analyzer.listResources());
 w.analyzer.use(Webcheck.middlewares.analyzer.statusHTTP());
 
-//w.analyzer("http://hypeinnovation.com/", function(){
-w.analyzer("http://ruhmesmeile.com/", function(){
-  console.log("FERTIG!");
+w.analyzer("http://www.ccc.de/", function(){
   fs.writeFile("./log-analysis.json", JSON.stringify(w.getAnalysis(),null, 2), function(err){
-    console.log("gespeichert...");
+    console.log("saved analysis");
   });
   fs.writeFile("./log-report.json", JSON.stringify(w.getReverseReport(),null, 2), function(err){
-    console.log("gespeichert...");
+    console.log("saved reverse report");
   });
   //console.log();
 });
