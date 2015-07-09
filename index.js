0d039def7c711d0e7118832e1ede75be61964e6f
Arne Schubert <atd.schubert@gmail.com>
2015-07-09 20:02:51 +0200

    #36: blueprint

--- index.js
+++ index.js
@@ -1,270 +1,163 @@
-"use strict";
+/*jslint node:true */
 
-var Crawler = require("crawler").Crawler;
-var util = require("util");
-var async = require("async");
-var EventEmitter = require("events").EventEmitter;
-var _ = require("underscore");
-var url = require("url");
+/**
+ * Webcheck core
+ * @author Arne Schubert <atd.schubert@gmail.com>
+ */
 
-var shortenURL = function(url){
-  if(typeof url != "string") throw new Error("URL must be a string!");
-  return url.split("#")[0];
+'use strict';
+
+var async = require('async');
+var request = require('request');
+var winston = require('winston');
+var EventEmitter = require('events').EventEmitter;
+var util = require('util');
+var cheerioize = require('./cheerioize');
+
+var pkg = require('./package.json');
+
+var std = {
+    headers: {
+        'User-Agent': pkg.name + ' ' + pkg.version
+    },
+    concurrency: 5
 };
 
-var Webcheck = exports = module.exports = function Webcheck(params) { // params = {url:""}
+/**
+ * Webcheck constuctor
+ * @param opts
+ * @constructor
+ */
+var Webcheck = function (opts) {
+    var drained, queue, self, sleepingQueue;
 
-  EventEmitter.call(this);
-  
-  var results = {};
-  var self = this;
-  var start;
-  
-  var queued = {};
-  
-  var analyzerMiddlewares = [], reporterMiddlewares = [];
-  
-  params = params || {};
-
-  _.extend(params, {
-    maxConnections: 10,
-    timeout: 60000,
-    userAgent: "node-webcheck",
-    retries: 3,
-    retryTimeout: 10000,
-    forceUTF8: true
-  });
-  
-  var crawler = new Crawler({
-    "maxConnections": params.maxConnections,
-    "timeout": params.timeout,
-    "userAgent": params.userAgent,
-    "retries": params.retries,
-    "retryTimeout": params.retryTimeout,
-    "forceUTF8": params.forceUTF8,
-    "followRedirect":false,
-    //"jQuery":false,
-    "callback": function(error,result,$) {
-      if(error) { self.emit("error", error); self.emit("analyzerError", error);}
-      if(!result) return;
-      //if(queued[result.request.href]) return;
-      if(results[result.request.href]) return;
-      
-      var ro = new Resource(result);
-      queued[result.request.href] = true;
-      async.applyEach(analyzerMiddlewares, ro, function callback(err){
-        if(err) console.error(err);
-        ro.clean();
-      });
-    },
-    "onDrain": function(){ // no arguments!
-      self.emit("finishAnalyzer", {timestamp: Date.now(), analysis: self.getAnalysis()});
+    if (!this) {
+        return new Webcheck(opts);
     }
-  });
-  
-  var Resource = function(result){
-    var ro = this;
-    
-    results[result.request.href] = this;
-    
-    var analysis = {};
-    var report = {};
-    var resources = {}; // {"http://url": count}
-    var referrers = {}; // {"http://url": count}
-    var roURL = result.request.href;
-    
-    ro.getResult = function(){
-      return result;
-    };
-    ro.getURL = function(){
-      return roURL;
-    };
-    ro.getWebcheck = function(){
-      return self;
-    };
-    ro.addAnalysis = function(hash, data) {
 
-      analysis[hash] = data;
-      
-      self.emit("addAnalysis", {name:hash, data:data, resource:ro});
-      return ro;
+    opts = opts || {};
+    self = this;
+    sleepingQueue = [];
+    queue = async.queue(function (task, callback) {
+        var options, req;
+
+        options = {
+            uri: task.url,
+            headers: task.headers || opts.headers || std.headers,
+            task: task
+        };
+
+        req = self.request(options)
+            .on('response', function (response) {
+                var i, obj;
+                obj = {
+                    url: task.url,
+                    request: req,
+                    response: response
+                };
+                obj.getCheerio = function getCheerio(cb) {
+                    cheerioize(response, cb);
+                };
+                for (i = 0; i < self.middlewares.length; i += 1) {
+                    self.middlewares[i].call(self, obj);
+                }
+                callback();
+            })
+            .on('error', function (err) {
+                self.logger.error('Error in request', err);
+                callback(err);
+            });
+        req.task = task;
+        // task.headers, task.url
+    }, opts.concurrency || std.concurrency);
+
+    queue.drain = function () {
+        drained = true;
+        self.emit('drain');
     };
-    ro.removeAnalysis = function(hash) {
-      analysis[hash] = null;
-      return ro;
-    };
-    ro.getAnalysis = function(optHash) {
-      if (optHash) return analysis[optHash];
-      return analysis;
-    };
-    ro.addReport = function(level, name, data) {
-      if(!report[name]) report[name] = {};
-      report[name][level] = data; // recommandations for data: {message: "", test: "Name of the test"}
-      self.emit("addReport", {level:level, name:name, data:data, resource:ro});
-      return ro;
-    };
-    ro.getReport = function(optHash, optLevel) {
-      if(optLevel) return report[optHash] ? report[optHash][optLevel] : {};
-      if(optHash) return report[optHash] || {};
-      return report;
-    };
-    ro.follow = function(href) {
-      href = shortenURL(href);
-      ro.addResource(href);
-      if(!queued[href] && href.substr(0,4)=="http") { // TODO maybe replace with url.format()[protocol]...
-        queued[href] = true;
-        self.queue(href);
-      }
-      return ro;
-    };
-    ro.addResource = function(href) { // TODO: absolute url!
-      href = shortenURL(href);
-      if(resources[href]) resources[href]++;
-      else resources[href] = 1;
-      
-      var referrer = self.getResource(href);
-      if(referrer) referrer.addReferrer(ro.getURL());
-      else self.on("resource:"+href, function(referrer){
-        referrer.addReferrer(ro.getURL());
-      });
-      
-      return ro;
-    };
-    ro.getResources = function() {
-      return resources;
-    };
-    ro.getReferrers = function() {
-      return referrers;
-    };
-    ro.addReferrer = function(href) {
-      href = shortenURL(href);
-      if(referrers[href]) referrers[href]++
-      else referrers[href] = 1;
-      return ro;
-    };
-    ro.clean = function(){ // procedure to free memory
-      result = false;
-      ro.follow = ro.addResource;
-      return ro;
-    };
-    
-    self.emit("resource", ro);
-    self.emit("resource:"+result.request.href, ro); 
-  };
-  //: Setter, getter for Webcheck
-  
-  this.getResources = function(){
-    return results;
-  };
-  this.getReport = function(optHash){
-    if(optHash) return this.getResource(optHash).getReport();
-    var hash;
-    var res = {};
-    for (hash in results) {
-      res[hash] = results[hash].getReport();
-    }
-    return res;
-  };
-  this.getReverseReport = function(optHash){
-    var tmp = this.getReport(optHash);
-    var url, name, level;
-    var res = {};
-    for (url in tmp) {
-      for(name in tmp[url]){
-        for(level in tmp[url][name]) {
-          res[level] = res[level] || {};
-          res[level][name] = res[level][name] || {};
-          res[level][name][url] = tmp[url][name][level];
+
+    /**
+     * A logger instantiated from winston
+     * @type {*|exports|ProfileHandler.logger}
+     */
+    this.logger = opts.logger || winston;
+    /**
+     * Request module to make requests with
+     * @type {*|responseToJSON.request|request|exports|Querystring.request|Multipart.request}
+     */
+    this.request = opts.request || request;
+    /**
+     * List of used middlewares
+     * @type {Array}
+     */
+    this.middlewares = [];
+
+    /**
+     * Object to write in reports
+     * @type {{}}
+     */
+    this.report = {};
+
+    /**
+     * Add middlewares to webcheck
+     * @param middleware {function}
+     */
+    this.use = function (middleware) {
+        this.middlewares.push(middleware);
+        if (typeof middleware.init === 'function') {
+            middleware.init(this);
         }
-      }
-    }
-    return res;
-  };
-  this.getAnalysis = function(optHash){
-    if(optHash) return this.getResource(optHash).getAnalysis();
-    var hash;
-    var res = {};
-    for (hash in results) {
-      res[hash] = results[hash].getAnalysis();
-    }
-    return res;
-  };
-  this.getResource = function(url){
-    url = shortenURL(url);
-    return results[url];
-  };
-  
-  //: Analyzer
-  this.analyzer = function(opts, cb){ // opts = {maxConnections:""...}
-    var i;
-    for (i=0; i<arguments.length; i++) {
-      switch (typeof arguments[i]) {
-        case "object":
-          opts = arguments[i];
-          break;
-        case "function":
-          cb = arguments[i];
-      }
-    }
-    if(!cb || typeof cb !== "function") cb = function(){};
-  
-    //EventEmitter.call(this);
-    self.emit("startAnalyzer", {timestamp: Date.now()});
-    if(opts) self.queue(opts);
-    self.on("finishAnalyzer", cb);
-  };
-  //util.inherits(this.analyzer, EventEmitter);
-  //: End of Analyzer
-  
-  //: Reporter
-  this.reporter = function(opts, cb){ 
-    var i;
-    for (i=0; i<arguments.length; i++) {
-      switch (typeof arguments[i]) {
-        case "object":
-          opts = arguments[i];
-          break;
-        case "function":
-          cb = arguments[i];
-      }
-    }
-    if(!cb || typeof cb !== "function") cb = function(){};
-    
-    var hash;
-    self.emit("startReporter", {timestamp: Date.now()});
-    
-    async.each(results, function(ro, cb){
-      async.applyEach(reporterMiddlewares, ro, function(err){
-        cb(err);
-      });
-    }, function(err){
-      var r = self.getReport();
-      self.emit("finishReporter", {report: r, timestamp:Date.now()});
-      if(err) {self.emit("error", err); self.emit("reporterError", err);}
-      cb(err, r, self.getReverseReport());
-    });
-    
+        this.emit('use', middleware);
+    };
+    /**
+     * Crawl a resource
+     * @param url {string}
+     * @param options {{}}
+     * @returns {self}
+     */
+    this.crawl = function (url, options) {
+        options = options || {};
 
-    
-  };
-  //util.inherits(this.analyzer, EventEmitter);
-  //: End of Reporter
-  
-  this.analyzer.use = function(middleware){
-    
-    analyzerMiddlewares.push(middleware);
-    return self.analyzer;
-  };
-  this.reporter.use = function(middleware){
-    reporterMiddlewares.push(middleware);
-    //self.reporter.emit("use", middleware);
-    return self.reporter;
-  };
-  this.queue = function(){crawler.queue.apply(crawler, arguments);};
-  
-  this.setMaxListeners(0);
+        if (!url) {
+            return self.logger.error('No url specified!', new Error('No url specified!'));
+        }
+
+        var caller = function () {
+            drained = false;
+            self.emit('run', {url:url, options:options, fn: caller});
+            queue.push({
+                url: url,
+                headers: options.headers,
+                options: options
+            });
+        };
+
+        this.emit('crawl', {url:url, options:options, fn: caller});
+
+        if (options.sleep) {
+            sleepingQueue.push(caller);
+            setTimeout(function () {
+                sleepingQueue.splice(sleepingQueue.indexOf(caller), 1)();
+            }, options.sleep);
+        } else {
+            caller();
+        }
+        return this;
+    };
+
+    /**
+     * Returns the status of webcheck
+     * @returns {{queueConcurrency: (number|webcheck.concurrency|q.concurrency), sleepingQueue: Number, drained: boolean}}
+     */
+    this.getStatus = function () {
+        return {
+            queueConcurrency: opts.concurrency || std.concurrency,
+            sleepingQueue: sleepingQueue.length,
+            drained: drained
+        };
+    };
 };
 
 util.inherits(Webcheck, EventEmitter);
 
-Webcheck.middlewares = require("./lib/middlewares");
+module.exports = Webcheck;
